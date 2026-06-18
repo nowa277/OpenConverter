3b4dd905719e1a967e4fdc019544f285d1015e18
nowa277 <sesleycheung@gmail.com>
2026-06-18 12:49:39 +0800

    feat(android): OpenConverterApp installs CrashReporter on startup

--- android/app/src/main/kotlin/com/openconverter/app/OpenConverterApp.kt
+++ android/app/src/main/kotlin/com/openconverter/app/OpenConverterApp.kt
@@ -1,5 +1,12 @@
 package com.openconverter.app
 
 import android.app.Application
+import com.openconverter.app.crash.CrashReporter
 
-class OpenConverterApp : Application()
+class OpenConverterApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Install as early as possible so any later init failure is captured.
+        CrashReporter.install(this)
+    }
+}
