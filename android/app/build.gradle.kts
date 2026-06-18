51d20d52ba2d5b372d0321280215193a0811a53e
nowa277 <sesleycheung@gmail.com>
2026-06-18 08:20:31 +0800

    chore(android): bump versionName 0.3.2 -> 0.3.3, versionCode 2 -> 3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.openconverter.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.3.2"
+        versionCode = 3
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
