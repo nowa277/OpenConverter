8b8ec9f086c0dcd2e68c26a904e3ed966c9851ff
nowa277 <sesleycheung@gmail.com>
2026-07-03 21:07:50 +0800

    chore(release): bump version to 0.3.4 (desktop) and 1.1.2 (android)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.openconverter.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
